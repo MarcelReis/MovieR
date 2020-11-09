b5ba06e03036b713a952ff128753b2fd00d46483
Marcelo Reis <oi@marcelreis.dev>
2020-11-08 23:24:17 -0300

    fix: missing lin to home in the appbar

--- src/components/appbar/appbar.tsx
+++ src/components/appbar/appbar.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
+import { Link as RouterLink, useLocation } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
+
 import BaseAppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import InputBase from "@material-ui/core/InputBase";
-import { useLocation } from "react-router-dom";
+import Box from "@material-ui/core/Box";
 
 import MenuIcon from "@material-ui/icons/Menu";
 import SearchIcon from "@material-ui/icons/Search";
@@ -32,6 +34,8 @@ const useStyles = makeStyles((theme) => ({
   },
   title: {
     flexGrow: 1,
+    textDecoration: "none",
+    color: theme.palette.grey[900],
   },
 }));
 
@@ -71,9 +75,9 @@ function AppBar() {
                 <MenuIcon />
               </IconButton>
 
-              <Typography variant="h6" className={classes.title}>
-                MovieR
-              </Typography>
+              <RouterLink className={classes.title} to="/">
+                <Typography variant="h6">MovieR</Typography>
+              </RouterLink>
             </>
           )}
 
