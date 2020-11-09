e3ef4036966765093b2b55f38eb581256916409d
Marcelo Reis <oi@marcelreis.dev>
2020-11-09 02:03:58 -0300

    feat: better loading results

--- src/components/searchList/searchList.tsx
+++ src/components/searchList/searchList.tsx
@@ -10,6 +10,7 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import ListItemText from "@material-ui/core/ListItemText";
 import Paper from "@material-ui/core/Paper";
+import Skeleton from "@material-ui/lab/Skeleton";
 
 import imdbAPI from "../../api";
 import { BasicInfoResult } from "../../api/types";
@@ -49,6 +50,7 @@ type PropsType = {
 
 function SearchList(props: PropsType) {
   const [results, setResults] = useState<BasicInfoResult[] | null>([]);
+  const [loading, setLoading] = useState(false);
 
   const classes = useStyles();
 
@@ -56,8 +58,13 @@ function SearchList(props: PropsType) {
     if (props.searchString === "") {
       debounceSearch.clear();
       setResults(null);
+      setLoading(false);
     } else {
-      debounceSearch(props.searchString, setResults);
+      setLoading(true);
+      debounceSearch(props.searchString, (result: BasicInfoResult[] | null) => {
+        setResults(result);
+        setLoading(false);
+      });
     }
   }, [props.searchString]);
 
@@ -65,7 +72,42 @@ function SearchList(props: PropsType) {
     debounceSearch.clear();
   }, []);
 
-  return props.open ? (
+  if (loading) {
+    return (
+      <Paper className={classes.root} elevation={0}>
+        <List>
+          <ListItem>
+            <ListItemAvatar>
+              <Skeleton variant="circle" width={40} height={40} />
+            </ListItemAvatar>
+            <ListItemText>
+              <Skeleton variant="text" />
+            </ListItemText>
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem>
+            <ListItemAvatar>
+              <Skeleton variant="circle" width={40} height={40} />
+            </ListItemAvatar>
+            <ListItemText>
+              <Skeleton variant="text" />
+            </ListItemText>
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem>
+            <ListItemAvatar>
+              <Skeleton variant="circle" width={40} height={40} />
+            </ListItemAvatar>
+            <ListItemText>
+              <Skeleton variant="text" />
+            </ListItemText>
+          </ListItem>
+        </List>
+      </Paper>
+    );
+  }
+
+  return results ? (
     <Paper className={classes.root} elevation={0}>
       <List>
         {results?.map((movie, index) => (
